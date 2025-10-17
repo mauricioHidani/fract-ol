8dfdc40b2f6f04637322b837b7cdacc1373776a9
mauricioHidani <mhidani@student.42org.br>
2025-10-17 13:20:52 -0300

    feat [libft]: string number compare

---
+++ libft/ft_strncmp.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/17 09:37:53 by mhidani           #+#    #+#             */
+/*   Updated: 2025/10/17 09:43:35 by mhidani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fractol.h"
+
+int	ft_strncmp(char *str1, char *str2, size_t n)
+{
+	size_t	i;
+
+	if (n == 0)
+		return (0);
+	i = 0;
+	while (str1[i] && str2[i] && str1[i] == str2[i] && i < (n - 1))
+		i++;
+	return ((unsigned char)str1[i] - (unsigned char)str2[i]);
+}
