e481f1730ca4cadfc409f0edf6ca019ce500908a
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2025-10-24 15:34:50 -0300

    feat [libft]: ft_strjoin

---
+++ libft/ft_strjoin.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/19 10:21:21 by mhidani           #+#    #+#             */
+/*   Updated: 2025/10/19 11:00:36 by mhidani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*new;
+	size_t	i;
+	size_t	s1_len;
+	size_t	s2_len;
+
+	if (!s1 || !s2)
+		return (NULL);
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	new = malloc(s1_len + s2_len + 1);
+	if (!new)
+		return (NULL);
+	i = 0;
+	while (*s1)
+		new[i++] = *s1++;
+	while (*s2)
+		new[i++] = *s2++;
+	new[i] = '\0';
+	return (new);
+}
